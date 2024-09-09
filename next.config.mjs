b9be431525c12a42f47dab9402a806a358c6d19b
iamdarwinsai <darushyam143@gmail.com>
2024-09-09 16:26:16 +0530

    fixed some routes

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ const nextConfig = {
           {
             key: "Content-Security-Policy",
             value:
-              "connect-src 'self' https://api.dscvr.one https://api1.stg.dscvr.one https://*.helius-rpc.com https://*.solana.com/ https://*.helius-rpc.com https://files.edgestore.dev/*; script-src 'self' 'wasm-unsafe-eval' 'unsafe-inline' 'unsafe-eval'",
+              "connect-src 'self' https://api.dscvr.one https://api1.stg.dscvr.one https://*.helius-rpc.com https://*.solana.com/ https://*.helius-rpc.com https://files.edgestore.dev/* https://tipblink.xyz/api/*",
           },
         ],
       },
