e39fba535cfeb5000db21a34487cfb3188170ae0
iamdarwinsai <darushyam143@gmail.com>
2024-09-09 16:38:11 +0530

    femoved all the changes

--- next.config.mjs
+++ next.config.mjs
@@ -3,20 +3,6 @@ const nextConfig = {
   images: {
     domains: ["files.edgestore.dev", "raw.githubusercontent.com"],
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value:
-              "connect-src 'self' https://api.dscvr.one https://api1.stg.dscvr.one https://*.helius-rpc.com https://*.solana.com/ https://*.helius-rpc.com https://files.edgestore.dev/* https://tipblink.xyz/api/*",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
